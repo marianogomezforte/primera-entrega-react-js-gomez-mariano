5cffec16dbd3f25cc1b6df03b31dd3eff783476c
mariano gomez forte <gomezfortem@gmail.com>
2023-07-31 20:54:27 -0300

    actualizo cambios para probar deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer saludo={"Hola Mundo!"}/>
+      <ItemListContainer saludo={"Hola Mundo"}/>
     </div>
   );
 }
